f6dcec2a2b5b58dfcc90a81be0eea7694600c0e9
JohnzExor <jimenojohnzyll@gmail.com>
2023-05-25 19:58:21 +0800

    api connects to mysql database workbench

---
+++ api.py
@@ -0,0 +1,10 @@
+from flask import Flask, make_response, jsonify, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] = "root"
+app.config["MYSQL_PASSWORD"] = "root"
+app.config["MYSQL_DB"] = "rest_db"
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+mysql = MySQL(app)
