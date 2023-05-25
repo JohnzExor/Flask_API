c9752afc3faf30baf1276d32a26a894607668994
JohnzExor <jimenojohnzyll@gmail.com>
2023-05-25 20:30:08 +0800

    added a few table functions

--- api.py
+++ api.py
@@ -8,3 +8,21 @@ app.config["MYSQL_PASSWORD"] = "root"
 app.config["MYSQL_DB"] = "rest_db"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 mysql = MySQL(app)
+
+def get_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
+
+@app.route("/tables", methods=["GET"])
+def show_tables():
+    return make_response(jsonify(get_fetch("show tables")))
+
+@app.route("/tables/<string:table>", methods=["GET"])
+def select_table(table):
+    return make_response(jsonify(get_fetch(f"select * from {table}")))
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+from api import get_fetch
+
+class test(unittest.TestCase):
+    def test_getfetch(self):
+        self.assertEqual(
+            
+        )
