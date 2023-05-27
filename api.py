b6db1582db7bb3d54ffe40757ad7b692ed2e9e2e
JohnzExor <jimenojohnzyll@gmail.com>
2023-05-27 09:07:35 +0800

    added get and post methods

--- api.py
+++ api.py
@@ -16,6 +16,7 @@ def get_fetch(query):
     cur.close()
     return data
 
+#GET
 @app.route("/tables", methods=["GET"])
 def show_tables():
     return make_response(jsonify(get_fetch("show tables")))
@@ -23,6 +24,28 @@ def show_tables():
 @app.route("/tables/<string:table>", methods=["GET"])
 def select_table(table):
     return make_response(jsonify(get_fetch(f"select * from {table}")))
+
+@app.route("/tables/<string:table>/<int:id>", methods=["GET"])
+def select_id(table, id):
+    return make_response(jsonify(get_fetch(f"select * from {table} where id='{id}'")))
+
+#POST
+@app.route("/tables/students/add/<int:id>", methods=["POST"])
+def add_student(id):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    last_name = info["last_name"]
+    first_name = info["first_name"]
+    subjects_id = info["subjects_id"]
+    query = "insert into students values(%s, %s, %s, %s)"
+    cur.execute(query, (id, last_name, first_name, subjects_id))
+    mysql.connection.commit()
+    row_changes = cur.rowcount
+    cur.close()
+    return make_response(jsonify({"Message": "Added Succesfully", "Row changes": row_changes}))
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
