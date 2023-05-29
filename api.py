181075f3390bf2589abe28c3b2053b1fee2e2420
JohnzExor <jimenojohnzyll@gmail.com>
2023-05-29 10:07:54 +0800

    added required api key

--- api.py
+++ api.py
@@ -1,5 +1,10 @@
-from flask import Flask, make_response, jsonify, request
+from functools import wraps
+from flask import Flask, make_response, jsonify, request, abort
 from flask_mysqldb import MySQL
+import secrets
+
+api_key = secrets.token_hex(16)
+print(f"API KEY: {api_key}")
 
 app = Flask(__name__)
 app.config["MYSQL_HOST"] = "localhost"
@@ -7,39 +12,48 @@ app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "root"
 app.config["MYSQL_DB"] = "rest_db"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+app.config["API_KEY"] = api_key
 mysql = MySQL(app)
+
+def api_key_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        api_key = request.headers.get("API-Key")
+        if api_key != app.config["API_KEY"]:
+            abort(401)  # Unauthorized
+        return f(*args, **kwargs)
+    return decorated_function
 
 #GET
 def get_fetch(query):
-    cur = mysql.connection.cursor()
-    cur.execute(query)
-    data = cur.fetchall()
-    cur.close()
-    return data
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data = cur.fetchall()
+        cur.close()
+        return make_response(jsonify(data), 200)
+    except Exception as e:
+        return make_response(jsonify({"Error": str(e)}), 400)
+
 
 @app.route("/tables", methods=["GET"])
+@api_key_required
 def show_tables():
-    try:
-        return make_response(jsonify(get_fetch("show tables")), 200)
-    except Exception as e:
-        return make_response(jsonify({"Error": str(e)}), 400)
+    return get_fetch("Show tables")
 
 @app.route("/tables/<string:table>", methods=["GET"])
+@api_key_required
 def select_table(table):
-    try:
-        return make_response(jsonify(get_fetch(f"select * from {table}")), 200)
-    except Exception as e:
-        return make_response(jsonify({"Error": str(e)}), 400)
+    return get_fetch(f"select * from {table}")
 
 @app.route("/tables/<string:table>/<int:id>", methods=["GET"])
+@api_key_required
 def select_id(table, id):
-    try:
-        return make_response(jsonify(get_fetch(f"select * from {table} where id='{id}'")), 200)
-    except Exception as e:
-        return make_response(jsonify({"Error": str(e)}), 400)
+    return get_fetch(f"select * from {table} where id='{id}'")
 
 #POST
 @app.route("/tables/students/<int:id>", methods=["POST"])
+@api_key_required
 def add_student(id):
     try:
         cur = mysql.connection.cursor()
@@ -55,23 +69,10 @@ def add_student(id):
         return make_response(jsonify({"Message": "Added Succesfully", "Row changes": row_changes}), 201)
     except Exception as e:
         return make_response(jsonify({"Error": str(e)}), 400)
-
-#DELETE
-@app.route("/tables/students/<int:id>", methods=["DELETE"])
-def delete_student(id):
-    try:
-        cur = mysql.connection.cursor()
-        query = "delete from students where id=%s"
-        cur.execute(query, (id,))
-        mysql.connection.commit()
-        row_changes = cur.rowcount
-        cur.close()
-        return make_response(jsonify({"Message": "Has been deleted successfully", "Row changes": row_changes}), 200)
-    except Exception as e:
-        return make_response(jsonify({"Error": str(e)}), 400)
-
+    
 #PUT
 @app.route("/tables/students/<int:id>", methods=["PUT"])
+@api_key_required
 def update_student(id):
     try:
         cur = mysql.connection.cursor()
@@ -87,7 +88,21 @@ def update_student(id):
         return make_response(jsonify({"Message": "PUT Success", "Row changes": row_changes}))
     except Exception as e:
         return make_response(jsonify({"Error": str(e)}), 400)
-
+    
+#DELETE
+@app.route("/tables/students/<int:id>", methods=["DELETE"])
+@api_key_required
+def delete_student(id):
+    try:
+        cur = mysql.connection.cursor()
+        query = "delete from students where id=%s"
+        cur.execute(query, (id,))
+        mysql.connection.commit()
+        row_changes = cur.rowcount
+        cur.close()
+        return make_response(jsonify({"Message": "Has been deleted successfully", "Row changes": row_changes}), 200)
+    except Exception as e:
+        return make_response(jsonify({"Error": str(e)}), 400)
 
 if __name__ == "__main__":
     app.run(debug=True)
